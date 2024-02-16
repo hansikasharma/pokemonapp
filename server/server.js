6d435633ddaace359b4138b302fbb528153c9c37
Hansika <77491944+hansikasharma@users.noreply.github.com>
2024-02-16 12:37:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const clientOptions = { serverApi: { version: '1', strict: true, deprecationErro
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["https://pokemonapp-beige.vercel.app","http:localhost:5173"],
+    origin: ["https://pokemonapp-beige.vercel.app","http://localhost:5173"],
     methods: ["POST", "GET"],
     credentials: true
   }
